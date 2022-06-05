515529fcaa3a05aa27a1794f34b9932b4378f660
Will Beebe <will@containerlabs.io>
2022-06-05 16:25:59 -0700

    using module monorepo (#2)

--- src/apis.tf
+++ src/apis.tf
@@ -1,5 +1,5 @@
 module "apis" {
-  source = "../../../provisioners/terraform/modules/gcp-apis"
+  source = "github.com/massdriver-cloud/terraform-modules//google-enable-apis?ref=9201b9f"
   services = [
     # needed for MD alarms
     "monitoring.googleapis.com"
